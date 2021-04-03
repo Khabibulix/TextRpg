940de7f77a98910db8a0ce8d50c3deef4435c0ca
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2021-04-03 11:13:58 +0200

    Moving method.
    Print description, and current location.

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-    //DOM GETTERS
+//DOM GETTERS
 var konsole= document.querySelector(".console");
 var informations = document.querySelector(".informationsJeu");
 var submitBtn = document.getElementById("submitButton");
 var position = document.getElementById("lieuActuel");
 
-    //Classes
-    class Lieu{
-        constructor(nom, nord, sud, ouest, est, description){
+class Lieu{
+        constructor(nom, direction){
             this.nom = nom;
-            this.nord = nord;
-            this.sud = sud;
-            this.ouest = ouest;
-            this.est = est;
-            this.description = description;
-           
+            this.direction = direction;
+            }    
+            goingTo(origine, destination){
+                origine = this.nom;
+                destination = this.direction;
+                position.innerText = destination;
+                informations.value = escalier.description;
         }
-    }
+}
     
-    //Lieux
-    var chambreSale = new Lieu("Chambre Sale", false, "Cage d'Escalier", false, false);
+//Lieux
+    var chambreSale = new Lieu("Chambre Sale", "Cage d'Escalier");
     chambreSale.description = "Vous vous trouvez dans une chambre dégueulasse , au sol jonché de paquets de chips vides et de canettes de bière bon marché. C'est ici que vous habitez, et comme chaque jour depuis bien longtemps vous vous rendez au travail, en traînant les pieds. Une seule ouverture dans la pièce, au sud, une porte en bois décrépie et une poignée se vautrant à moitié. Que voulez-vous faire?"
-    var escalier = new Lieu("Cage d'Escalier", "Chambre Sale", false, false, false);
+    var escalier = new Lieu("Cage d'Escalier", "Chambre Sale");
     escalier.description = "Vous voici dans la cage d'escalier de votre immeuble, vous êtes au dernier étage, au 3ème. Allez-vous tenter d'éviter votre commère de voisine en partant de suite ou allez-vous tester les limites de votre patience?"
     position.innerText = chambreSale.nom;
 
     
-    //MAIN
+//MAIN
 submitBtn.addEventListener("click", function(prompt){
     var prompt = konsole.value;
     if (prompt == "effacer"){clear();};
     if (prompt == "chier"){chier();};
     if (prompt == "dormir"){dormir();};
     if (prompt == "sud" && position.textContent == "Chambre Sale"){
-        position.innerText = chambreSale.sud;
-        informations.value = escalier.description;
+        chambreSale.goingTo(chambreSale, escalier);
     }
 });
 
 
 
-    //Function
-//Commandes
+//Function
 
 function chier(){
     clear();
